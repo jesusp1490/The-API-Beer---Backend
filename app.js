9372c1f5d35b9697d415158929d08541b86134c9
Jesus <jesusperez_14@hotmail.com>
2024-03-15 11:22:15 +0100

    Fixed server conection

--- app.js
+++ app.js
@@ -3,23 +3,16 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const beerRoutes = require('./src/api/routes/beerRoutes');
 const dotenv = require('dotenv');
+const connectDB = require('./src/utils/db');
 dotenv.config();
 const app = express();
+
 
 // Middleware para analizar el cuerpo de las solicitudes entrantes
 app.use(bodyParser.json());
 
 // Conexión a MongoDB
-mongoose.connect('mongodb://localhost:27017/beerDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-    console.log('Conectado a MongoDB');
-});
+connectDB();
 
 // Rutas de la API de cervezas
 app.use('/api/beers', beerRoutes);
--- src/utils/db.js
+++ src/utils/db.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const dataBase = await mongoose.connect(process.env.MONGO_URI, {
-        });
-
-        console.log(`MongoDB Conectado: ${dataBase.connect.host}`);
+        const connection = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Conectado: ${connection.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
